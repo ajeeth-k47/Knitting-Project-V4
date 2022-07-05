d89dd657c00751a452c29f9db24aaa708280cdff
Ajeeth <66105938+ajeeth-k47@users.noreply.github.com>
2022-07-05 20:58:06 +0530

    Implemented Billing and optional delete function

---
+++ server/models/ordermodel.js
@@ -0,0 +1,121 @@
+const mongoose = require('mongoose');
+const conn = require('./database');
+
+const yarnlistSchema = new mongoose.Schema({
+    yarn:String,
+    mill:String,
+    color:String,
+    weight:Number
+  });
+
+
+const orderlistSchema = new mongoose.Schema({
+    orderNo:String,
+    company:String,
+    orderDate:Date,
+    supplier:String,
+    yarnCount:[
+        yarnlistSchema
+    ],
+    yarnWeight:Number,
+    balanceWeight:Number,
+    status:String
+});
+
+const programlistSchema = new mongoose.Schema({
+  orderNo:String,
+  programNo:String,
+  programDate:Date,
+  supplier:String,
+  fabricType:String,
+  dia:Number,
+  gauge:Number,
+  loopLength:String,
+  designNo:String,
+  gsm:String,
+  Weight:Number,
+  targetDate:String,
+  price:Number,
+  tax:String,
+  rolls:Number,
+  production:Number,
+  balance:Number,
+  deliveredWgt:Number,
+  deliveredrolls:Number,
+  deliveryStatus:String
+})
+
+const deliveredProgramlistSchema = new mongoose.Schema({
+  orderNo:String,
+  programNo:String,
+  deliveryNo:String,
+  designNo:String,
+  supplier:String,
+  deliveryDate:Date,
+  weight:Number,
+  rollcount:Number,
+  totalPrice:Number
+})
+
+const returnyarnSchema = new mongoose.Schema({
+  orderNo:String,
+  returnWeight:Number,
+  returnDate:String,
+  reason:String
+})
+
+const timelineSchema = new mongoose.Schema({
+  orderNo:String,
+  reference:String,
+  datetime:String,
+  action:String,
+  description:String
+})
+
+
+const primarykeylookupSchema = new mongoose.Schema({
+   modelName:String,
+   lastPrimaryKey:String,
+   generatedDate:Date
+})
+
+
+const rollDetailSchema = new mongoose.Schema({
+  programNo:String,
+  designNo:String,
+  rollWeight:Number,
+  rollDate:Date
+})
+
+
+const billinglistSchema = new mongoose.Schema({
+  billNo:String,
+  billDate:Date,
+  orderNo:String,
+  programNo:String,
+  supplier:String
+})
+
+const orderlist= mongoose.orderDB.model('orderlist', orderlistSchema);
+const yarnlist= mongoose.orderDB.model('yarnlist', yarnlistSchema);
+const programlist = mongoose.orderDB.model('programlist', programlistSchema); 
+const returnyarn = mongoose.orderDB.model('returnyarn', returnyarnSchema); 
+const timeline = mongoose.orderDB.model('timeline', timelineSchema);
+const primarykeyLookup = mongoose.orderDB.model('primarykeyLookup',primarykeylookupSchema);
+const rollDetails = mongoose.orderDB.model('rollDetails',rollDetailSchema);
+const deliveredProgramlist = mongoose.orderDB.model('deliveredProgramlist',deliveredProgramlistSchema); 
+const billinglist = mongoose.orderDB.model('billinglist',billinglistSchema);
+
+module.exports = {
+  orderlist,
+  yarnlist,
+  programlist,
+  returnyarn,
+  timeline,
+  primarykeyLookup,
+  rollDetails,
+  deliveredProgramlist,
+  billinglist
+}
+
+
---
+++ server/routes/order.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const order = express.Router();
+const orderController = require('../controllers/orderController');
+
+
+order.get('/orderlist',orderController.orderpage);
+order.get('/programlist',orderController.knittingProgramPage);
+order.get('/deliverylist',orderController.viewDeliverylist);
+order.get('/billinglist',orderController.viewBilllist);
+
+order.post('/createorder',orderController.createorderpage);
+order.post('/neworder',orderController.addorder);
+order.post('/addorderprogram',orderController.addorderprogram);
+order.post('/addyarn',orderController.addyarn);
+order.post('/updateYarn',orderController.updateYarn);
+order.post('/editdeleteYarn',orderController.editdeleteYarn);
+order.post('/editYarn',orderController.editYarn);
+order.post('/deleteYarn',orderController.deleteYarn);
+order.post('/addProgram',orderController.addProgram);
+order.post('/returnYarnPage',orderController.returnYarnPage);
+order.post('/returnYarn',orderController.returnyarn);
+order.post('/activity',orderController.activitylog);
+order.post('/editSelectedYarn',orderController.editSelectedYarn);
+order.post('/searchbysupplier',orderController.searchBySupplier);
+order.post('/searchbydate',orderController.searchByDate);
+order.post('/searchByKnittingfeatures',orderController.searchByKnittingfeatures);
+order.post('/add_rolls',orderController.addRolls);
+order.post('/updateProgram',orderController.updateProgram);
+order.post('/view_rolls',orderController.viewRolls);
+order.post('/deliverProgram',orderController.deliverProgram);
+order.post('/view_deliveries',orderController.viewDeliveries);
+order.post('/billing',orderController.addBilling);
+order.post('/viewbilling',orderController.viewBilling);
+order.post('/viewDeliveryChalan',orderController.viewDeliveryChalan);
+order.post('/deleteOrder',orderController.deleteOrder);
+
+module.exports = order;
